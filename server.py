782a7ee1484fcb4a53082c0c557d7b124e4ee96c
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2024-02-14 10:15:25 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+# from playsound import playsound
+
+server = Flask(__name__)
+
+@server.route("/")
+def index(methods = ["GET", "POST"]):
+    print(request.method)
+    if request.method == "POST":
+        print("going to play the sound located at:", sound_file)
+        return
+
+    if request.method == "GET":
+        with open("index.html") as f:
+            content = f.read()
+            print(content)
+            return content
+        
+
+
+server.run("0.0.0.0", "3000", debug=True)
